a0b538d50777a1ba24853b430f2c256e943ba1bc
Dirb <clarissa.aurelia1001@gmail.com>
2025-08-02 13:16:47 +0700

    Landing page update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,11 +4,11 @@ import Link from "next/link";
 const Navbar = () => {
   return (
     <div className='py-10 px-15 font-mont flex items-center relative'>
-      <Link className=' text-2xl font-bold hover:text-[#C6AFFF] transition-colors duration-300' href=''>OneStopUMKM</Link>
+      <Link className=' text-xl font-bold hover:text-[#C6AFFF] transition-colors duration-300' href=''>OneStopUMKM</Link>
       <div className='flex items-center  gap-15 absolute left-1/2 transform -translate-x-1/2'>
-        <Link className='mx-4 font-normal text-lg hover:text-[#C6AFFF] transition-colors duration-300' href='/'>Home</Link>
-        <Link className='mx-4 text-lg hover:text-[#C6AFFF] transition-colors duration-300' href='/about'>Features</Link>
-        <Link className='mx-4 text-lg hover:text-[#C6AFFF] transition-colors duration-300' href='/contact'>Team</Link>
+        <Link className='mx-4 font-normal text-md hover:text-[#C6AFFF] transition-colors duration-300' href='/'>Home</Link>
+        <Link className='mx-4 text-md hover:text-[#C6AFFF] transition-colors duration-300' href='/features'>Features</Link>
+        <Link className='mx-4 text-md hover:text-[#C6AFFF] transition-colors duration-300' href='/team'>Team</Link>
       </div>
       <Link href='/login' className='ml-auto px-7 bg-button text-white py-2 rounded-lg text-md hover:bg-opacity-80 hover:scale-105 transition-all duration-300'>
         Login
