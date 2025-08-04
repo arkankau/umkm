731e4b62a68b33d2ea0049693bc2d055e93d938e
Dirb <clarissa.aurelia1001@gmail.com>
2025-08-04 23:07:26 +0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  // Add these new configurations for static export
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  },
 };
 
 export default nextConfig;
